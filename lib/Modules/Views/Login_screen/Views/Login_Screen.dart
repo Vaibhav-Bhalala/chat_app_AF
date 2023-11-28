e3c455a9e1f622dafc056b21316fb5f322b6e597
r37 <vaibhavbhalala1212@gmail.com>
2023-11-28 16:35:03 +0530

    changes in login screen ui

--- lib/Modules/Views/Login_screen/Views/Login_Screen.dart
+++ lib/Modules/Views/Login_screen/Views/Login_Screen.dart
@@ -5,119 +5,175 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Screen"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                height: 400,
-                width: 500,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("lib/Modules/Assets/Images/Login.gif"),
-                        fit: BoxFit.cover)),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: "Enter Email",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Screen"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: 300,
+                  width: 500,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage("lib/Modules/Assets/Images/Login.gif"),
+                          fit: BoxFit.cover)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: "Enter Email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 40,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(8),
+                SizedBox(
+                  height: 10,
                 ),
-                child: Center(
-                  child: Text(
-                    "Log In",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white),
+                InkWell(
+                  onTap: () {},
+                  child: Ink(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ]),
+                    child: Container(
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Log In",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 40,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.circular(8),
+                SizedBox(
+                  height: 10,
                 ),
-                child: Center(
-                  child: Text(
-                    "Annoymous",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white),
+                InkWell(
+                  onTap: () {},
+                  child: Ink(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ]),
+                    child: Container(
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Annoymous",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 40,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.redAccent,
-                  borderRadius: BorderRadius.circular(8),
+                SizedBox(
+                  height: 10,
                 ),
-                child: Center(
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white),
+                InkWell(
+                  onTap: () {},
+                  child: Ink(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ]),
+                    child: Container(
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey),
+                        color: Colors.pink.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 40,
-                width: 150,
-                decoration: BoxDecoration(
-
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(8),
-                  image: DecorationImage(image: AssetImage("lib/Modules/Assets/Images/googlelogin.png"),fit: BoxFit.cover),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-            ],
+                InkWell(
+                  onTap: () {},
+                  child: Ink(
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 5),
+                        color: Colors.grey,
+                        blurRadius: 5,
+                      )
+                    ]),
+                    child: Container(
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "lib/Modules/Assets/Images/create-apple-google-signin-buttons-quick-dirty-way-google.png"),
+                            fit: BoxFit.fitWidth),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
